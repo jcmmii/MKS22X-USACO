a33b6e466994c9f9827d35c2ec8508f47a16335e
Justin Chen <jchen06@stuy.edu>
2019-03-10 22:05:34 -0400

    Finally finished reading in first line; prev commit did not read in last val

--- USACO.java
+++ USACO.java
@@ -2,41 +2,46 @@ import java.util.*;
 import java.io.*;
 
 public class USACO {
-  private int[][] Lake;
-
+  
   public static int bronze(String filename) throws FileNotFoundException {
     File fileIn = new File(filename);
     Scanner scan = new Scanner(fileIn);
 
-    int height = 1;
-    int width = 0;
-
     String firstLine = "";
-
     if (scan.hasNextLine()) {
       firstLine = scan.nextLine();
-      /*
     }
-    while (scan.hasNextLine()) {
-      String findWidth = scan.nextLine();
-      width = findWidth.length();
-      height = height + 1;
+    String[] numbersArray = firstLine.split(" ");
+    int index = 0;
+    int row = 0;
+    int col = 0;
+    int num = 0;
+    int elevation = 0;
+
+    while (index != 4) {
+      if (index == 0) row = Integer.parseInt(numbersArray[0]);
+      if (index == 1) col = Integer.parseInt(numbersArray[1]);
+      if (index == 2) elevation = Integer.parseInt(numbersArray[2]);
+      if (index == 3) num = Integer.parseInt(numbersArray[3]);
+      index++;
     }
 
-    Lake = new int[height][width];
-*/
-  }
-  System.out.println(firstLine);
-  return 1;
+    System.out.println(row);
+    System.out.println(col);
+    System.out.println(elevation);
+    System.out.println(num);
+    return 1;
 }
 
 //  public static int silver(String filename) {  }
+
+
 public static void main(String[] args) {
   try {
       bronze("makelake.1.in");
     }
   catch (FileNotFoundException e) {
-    
+
   }
 }
 }
