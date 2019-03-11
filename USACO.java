a6c58be55b881a2aceeeb3d8b41b9cb3b18e8956
Justin Chen <jchen06@stuy.edu>
2019-03-10 22:21:18 -0400

    Reads in lake, added a toString method

--- USACO.java
+++ USACO.java
@@ -2,22 +2,27 @@ import java.util.*;
 import java.io.*;
 
 public class USACO {
-  
+
   public static int bronze(String filename) throws FileNotFoundException {
+
     File fileIn = new File(filename);
     Scanner scan = new Scanner(fileIn);
 
+    //reads in first line
     String firstLine = "";
     if (scan.hasNextLine()) {
       firstLine = scan.nextLine();
     }
+    //makes an array of Strings; separates values based on spaces between
     String[] numbersArray = firstLine.split(" ");
+    //initialize vars
     int index = 0;
     int row = 0;
     int col = 0;
     int num = 0;
     int elevation = 0;
 
+    //parses Ints from the array of Strings
     while (index != 4) {
       if (index == 0) row = Integer.parseInt(numbersArray[0]);
       if (index == 1) col = Integer.parseInt(numbersArray[1]);
@@ -26,12 +31,31 @@ public class USACO {
       index++;
     }
 
-    System.out.println(row);
-    System.out.println(col);
-    System.out.println(elevation);
-    System.out.println(num);
+    int[][] Lake = new int[row][col];
+    for (int x = 0; x < Lake.length; x++) {
+      String newLine = scan.nextLine();
+      String[] parseLine = newLine.split(" ");
+      for (int y = 0; y < parseLine.length; y++) {
+        Lake[x][y] = Integer.parseInt(parseLine[y]);
+      }
+    }
+
+    System.out.println(toString(Lake));
+
+
     return 1;
 }
+
+    public static String toString(int[][] Lake) {
+      String ret = "";
+      for (int x = 0; x < Lake.length; x++) {
+        for (int y = 0; y < Lake[0].length; y++) {
+          ret = ret + Lake[x][y] + " ";
+        }
+        ret = ret + "\n";
+      }
+      return ret;
+    }
 
 //  public static int silver(String filename) {  }
 
